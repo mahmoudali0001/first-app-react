a772deed8e648ade50d4045d97fbd0bec29198c0
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-06-10 20:06:05 +0200

    use React.lazy in project

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 // import axios from "axios";
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import classes from "./Blog.css";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+// import NewPost from "./NewPost/NewPost";
 import NotFound404 from "../../components/NotFound404/NotFound404";
+// import asyncComponent from "../../hoc/asyncComponent";
+
+const AsyncNewPost = React.lazy(() => import("./NewPost/NewPost"));
 
 class Blog extends Component {
   state = {
-    auth: false,
+    auth: true,
   };
 
   render() {
+    console.log(AsyncNewPost);
+
     return (
       <div className={classes.Blog}>
         <header>
@@ -40,7 +45,14 @@ class Blog extends Component {
         </header>
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route
+              path="/new-post"
+              render={() => (
+                <Suspense fallback={<div>Loading.....!</div>}>
+                  <AsyncNewPost />
+                </Suspense>
+              )}
+            />
           ) : null}
           <Route path="/posts" component={Posts} />
 
